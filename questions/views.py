a218e2a36abd21a46066a6f918e48bc5cb852218
Vlad Damian <vld.damian@gmail.com>
2015-06-22 21:06:34 +0300

    first template

--- questions/views.py
+++ questions/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+from .models import *
 # Create your views here.
+
+
+def list_questionnaires(request):
+    questionnaires = Questionnaire.objects.all()
+
+    return render(request, "landing_page.html",
+                  {"questionnaires": questionnaires})
